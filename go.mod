53ac3fc4324c954c55b1cbc2325675b73c95770b
Nitin <ni2.khanna@gmail.com>
2021-08-27 10:34:08 -0700

    base install

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/papiguy/v1
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.14.1
+)
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	m, err := migrate.New(
+		"file://db/migrations",
+		"postgresql://localhost/hello?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := m.Up(); err != nil {
+		log.Fatal(err)
+	}
+}
